cc400b9e89f1c48b4b03fec15656613e5121c846
Wojciech Kumoń <wojtas626@gmail.com>
2017-01-17 20:45:56 +0100

    Added gradients

--- src/main/java/org/kris/oilsimulation/controller/pollutionmap/PollutionMapController.java
+++ src/main/java/org/kris/oilsimulation/controller/pollutionmap/PollutionMapController.java
@@ -1,5 +1,6 @@
 package org.kris.oilsimulation.controller.pollutionmap;
 
+import org.kris.oilsimulation.controller.Colors;
 import org.kris.oilsimulation.controller.util.WindowUtil;
 import org.kris.oilsimulation.model.automatonview.AutomatonView;
 
@@ -9,7 +10,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.stage.Window;
 
 import static org.kris.oilsimulation.controller.Colors.LAND_COLOR;
@@ -19,7 +24,15 @@ public class PollutionMapController {
   private static final String ICON_PATH = "view/img/icon.png";
 
   @FXML
-  private Canvas canvas;
+  private Canvas mapCanvas;
+  @FXML
+  private Canvas landPollutionGradient;
+  @FXML
+  private Canvas waterPollutionGradient;
+  @FXML
+  private Label maxIterationsLabel1;
+  @FXML
+  private Label maxIterationsLabel2;
 
   public static void showPollutionMap(Window mainWindow, AutomatonView automatonView) {
     Consumer<FXMLLoader> adjustFxml = fxmlLoader -> {
@@ -34,6 +47,8 @@ public class PollutionMapController {
   private void setAutomatonView(AutomatonView automatonView) {
     PollutionMap pollutionMap = getPollutionMap(automatonView);
     drawPollutionMap(pollutionMap);
+    drawGradients(pollutionMap.getMaxIterations());
+    setLabels(pollutionMap.getMaxIterations());
   }
 
   private PollutionMap getPollutionMap(AutomatonView automatonView) {
@@ -43,14 +58,14 @@ public class PollutionMapController {
   private void drawPollutionMap(PollutionMap pollutionMap) {
     int mapSize = pollutionMap.getCols();
     double cellSize = calculateCellSize(mapSize);
-    GraphicsContext graphics = canvas.getGraphicsContext2D();
+    GraphicsContext graphics = mapCanvas.getGraphicsContext2D();
     drawCells(graphics, pollutionMap, mapSize, cellSize);
     drawGrid(graphics, mapSize, cellSize);
   }
 
   private double calculateCellSize(int mapSize) {
-    double cellMaxWidth = canvas.getWidth() / mapSize;
-    double cellMaxHeight = canvas.getHeight() / mapSize;
+    double cellMaxWidth = mapCanvas.getWidth() / mapSize;
+    double cellMaxHeight = mapCanvas.getHeight() / mapSize;
 
     return cellMaxWidth <= cellMaxHeight ? cellMaxWidth : cellMaxHeight;
   }
@@ -66,7 +81,8 @@ public class PollutionMapController {
 
   private void drawCell(GraphicsContext graphics, PollutionMap pollutionMap,
                         double cellSize, int i, int j) {
-    graphics.setFill(getCellColor(pollutionMap.get(i, j), pollutionMap.getMaxIterations()));
+    Color cellColor = getCellColor(pollutionMap.get(i, j), pollutionMap.getMaxIterations());
+    graphics.setFill(cellColor);
     graphics.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
   }
 
@@ -82,5 +98,25 @@ public class PollutionMapController {
         graphics.strokeRect(i * cellSize, j * cellSize, cellSize, cellSize);
       }
     }
+  }
+
+  private void drawGradients(int maxIterations) {
+    drawRadialGradient(landPollutionGradient, Colors.LAND_COLOR);
+    drawRadialGradient(waterPollutionGradient, Colors.WATER_COLOR);
+  }
+
+  private void drawRadialGradient(Canvas canvas, Color color) {
+    GraphicsContext graphics = canvas.getGraphicsContext2D();
+    graphics.setFill(new LinearGradient(0, 0, 1, 1, true,
+        CycleMethod.REFLECT,
+        new Stop(0.0, Color.BLACK),
+        new Stop(1.0, color)));
+    graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+  }
+
+  private void setLabels(int maxIterations) {
+    String stringIterations = Integer.toString(maxIterations);
+    maxIterationsLabel1.setText(stringIterations);
+    maxIterationsLabel2.setText(stringIterations);
   }
 }
